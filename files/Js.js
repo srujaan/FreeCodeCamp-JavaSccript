cb63fc61a707dc3c0d3c549ffa6d5b49ce6bd5a8
srujan <srujans321@gmail.com>
2020-06-05 11:23:37 +0530

    largest number in suba array

--- files/Js.js
+++ files/Js.js
@@ -58,7 +58,7 @@ reverseString("hello")*/
 
 factorialize(5);*/
 
-function findLongestWordLength(str) {
+/*function findLongestWordLength(str) {
   var w = str.split()
   var maxLength = 0;
 
@@ -68,7 +68,24 @@ function findLongestWordLength(str) {
     }
   }
   return maxLength;
+}*/
+
+//findLongestWordLength("The quick brown fox jumped over the lazy dog");
+function largestOfFour(arr) {
+  var results = [];
+  for (var n = 0; n < arr.length; n++) {
+    var largestNumber = arr[n][0];
+    for (var sb = 1; sb < arr[n].length; sb++) {
+      if (arr[n][sb] > largestNumber) {
+        largestNumber = arr[n][sb];
+      }
+    }
+
+    results[n] = largestNumber;
+  }
+
+  return results;
 }
 
-findLongestWordLength("The quick brown fox jumped over the lazy dog");
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
 
