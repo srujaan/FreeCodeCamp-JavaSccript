301599fab8fe7c6153c524d1444f7fb7a9ad7961
srujan <srujans321@gmail.com>
2020-06-11 11:38:48 +0530

    slice and splice

--- files/Js.js
+++ files/Js.js
@@ -197,3 +197,12 @@ function titleCase(str) {
 
 titleCase("I'm a little tea pot");
 
+function frankenSplice(arr1, arr2, n) {
+  // It's alive. It's alive!
+  let localArray = arr2.slice();
+  for (let i = 0; i < arr1.length; i++) {
+    localArray.splice(n, 0, arr1[i]);
+    n++;
+  }
+  return localArray;
+}
