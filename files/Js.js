820787898ef9ea53ae6ccdf4d77a339687cc7bd7
srujan <srujans321@gmail.com>
2020-06-13 13:17:14 +0530

    mutation in internal arrays

--- files/Js.js
+++ files/Js.js
@@ -235,3 +235,11 @@ function getIndexToIns(arr3, num3) {
 
 getIndexToIns([40, 60, 43, 2, 87], 50);
 
+function mutation(arr) {
+  var test = arr[1].toLowerCase();
+  var target = arr[0].toLowerCase();
+  for (var i = 0; i < test.length; i++) {
+    if (target.indexOf(test[i]) < 0) return false;
+  }
+  return true;
+}
