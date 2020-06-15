b4b735b91ae3e591122a6d471e97c5d5242b644d
srujan <srujans321@gmail.com>
2020-06-15 11:16:51 +0530

    new keyword

--- files/Js.js
+++ files/Js.js
@@ -270,3 +270,12 @@ let dog = {
 
 dog.sayLegs();
 
+function Dog() {
+  this.name = "Rupert";
+  this.color = "brown";
+  this.numLegs = 4;
+}
+// Only change code below this line
+let hound = new Dog()
+
+
