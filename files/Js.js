4e270e6f60ac93763a8e5d9e566ce690e9535cf2
srujan <srujans321@gmail.com>
2020-06-15 11:05:23 +0530

    this method

--- files/Js.js
+++ files/Js.js
@@ -261,3 +261,12 @@ let dog = {
 };
 
 dog.sayLegs();
+
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+  sayLegs: function() {return "This dog has " + this.numLegs + " legs.";}
+};
+
+dog.sayLegs();
+
